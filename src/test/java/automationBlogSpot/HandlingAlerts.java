e785e17f19e39c8d769d54f9e609125237fa7929
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-28 22:58:44 +0530

    automationBlogSpot final Commit

--- src/test/java/automationBlogSpot/HandlingAlerts.java
+++ src/test/java/automationBlogSpot/HandlingAlerts.java
@@ -25,6 +25,7 @@ public class HandlingAlerts {
 
 	@Test(priority = 1)
 	public void sampleAlert() {
+		System.out.println("----Handling Normal Alerts----");
 		WebElement sampleAlert = driver.findElement(By.xpath("//button[@id='alertBtn']"));
 		sampleAlert.click();
 		Alert sa = driver.switchTo().alert();
@@ -34,23 +35,39 @@ public class HandlingAlerts {
 
 	@Test(priority = 2)
 	public void confirmationAlert() {
+		System.out.println("----Handling Confirmation Alerts----");
 		WebElement confirmAlert = driver.findElement(By.xpath("//button[@id='confirmBtn']"));
 		confirmAlert.click();
-		Alert sa = driver.switchTo().alert();
-		System.out.println(sa.getText());
+		Alert ca = driver.switchTo().alert();
+		System.out.println(ca.getText());
 		// Accept the Alert
-		sa.accept();
+		ca.accept();
 		String result = driver.findElement(By.xpath("//p[@id='demo']")).getText();
 		System.out.println(result);
 		
 		// Cancel the Alert
 //		sa.dismiss();
+//		String result = driver.findElement(By.xpath("//p[@id='demo']")).getText();
 //		System.out.println(result);
 	}
 
 	@Test(priority = 3)
 	public void promptAlert() {
-
+		System.out.println("----Handling Prompt Alerts----");
+		WebElement promptAlert = driver.findElement(By.xpath("//button[@id='promptBtn']"));
+		promptAlert.click();
+		Alert pa = driver.switchTo().alert();
+		System.out.println(pa.getText());
+		// Enter text and Accept the Alert
+		pa.sendKeys("Monkey D Luffy");
+		pa.accept();
+		String result = driver.findElement(By.xpath("//p[@id='demo']")).getText();
+		System.out.println(result);
+		
+		// Cancel the Alert
+//		sa.dismiss();
+//		String result = driver.findElement(By.xpath("//p[@id='demo']")).getText();
+//		System.out.println(result);
 	}
 
 	@AfterClass
