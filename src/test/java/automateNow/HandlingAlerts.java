08f8ba03e7a9372e8fded09ab915de958b7b889c
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-09 15:57:30 +0530

    Sample Alert

--- src/test/java/automateNow/HandlingAlerts.java
+++ src/test/java/automateNow/HandlingAlerts.java
@@ -2,6 +2,8 @@ package automateNow;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -22,15 +24,19 @@ public class HandlingAlerts {
 
 	@Test(priority = 1)
 	public void sampleAlert() {
-
+		// Click Alert Popup button
+		driver.findElement(By.xpath("//button[@id='alert']")).click();		
+		Alert sa = driver.switchTo().alert();
+		System.out.println(sa.getText());
+		sa.accept();
 	}
 
-	@Test(priority = 2)
+//	@Test(priority = 2)
 	public void confirmationAlert() {
 
 	}
 
-	@Test(priority = 3)
+//	@Test(priority = 3)
 	public void promptAlert() {
 
 	}
