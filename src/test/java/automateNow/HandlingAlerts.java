9636c7ba8dbc9e87181e80a7971e328a39cc82a4
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-09 16:06:56 +0530

    Fina\l Execution of Alerts

--- src/test/java/automateNow/HandlingAlerts.java
+++ src/test/java/automateNow/HandlingAlerts.java
@@ -25,20 +25,33 @@ public class HandlingAlerts {
 	@Test(priority = 1)
 	public void sampleAlert() {
 		// Click Alert Popup button
-		driver.findElement(By.xpath("//button[@id='alert']")).click();		
+		driver.findElement(By.xpath("//button[@id='alert']")).click();
 		Alert sa = driver.switchTo().alert();
 		System.out.println(sa.getText());
 		sa.accept();
 	}
 
-//	@Test(priority = 2)
+	@Test(priority = 2)
 	public void confirmationAlert() {
-
+		// Click Confirm Popup button
+		driver.findElement(By.xpath("//b[normalize-space()='Confirm Popup']")).click();
+		Alert ca = driver.switchTo().alert();
+		System.out.println(ca.getText());
+		ca.accept();
+		// Dismiss Alert
+//		ca.dismiss();
 	}
 
-//	@Test(priority = 3)
+	@Test(priority = 3)
 	public void promptAlert() {
-
+		// Click Prompt Popup button
+		driver.findElement(By.xpath("//b[normalize-space()='Prompt Popup']")).click();
+		Alert pa = driver.switchTo().alert();
+		pa.sendKeys("Uzumaki Naruto");
+		System.out.println(pa.getText());
+		pa.accept();
+		String resultText = driver.findElement(By.xpath("//p[@id='promptResult']")).getText();
+		System.out.println(resultText);
 	}
 
 	@AfterClass
