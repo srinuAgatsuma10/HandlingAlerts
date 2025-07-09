acbbc2cdadc9473fdf433b61d3f112ec4247a8e6
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-09 15:54:27 +0530

    automateNow setUp

---
+++ src/test/java/automateNow/HandlingAlerts.java
@@ -0,0 +1,42 @@
+package automateNow;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingAlerts {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://practice-automation.com/popups/");
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 1)
+	public void sampleAlert() {
+
+	}
+
+	@Test(priority = 2)
+	public void confirmationAlert() {
+
+	}
+
+	@Test(priority = 3)
+	public void promptAlert() {
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
